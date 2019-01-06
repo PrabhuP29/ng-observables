a9593c4b4b540eba2fb39157b9da730d59ff1cab
Prabhu Pattnaik <rudolph2006@gmail.com>
2019-01-06 17:23:32 -0500

    Added Observables
    Added Observables

--- observables-start/src/app/app.component.ts
+++ observables-start/src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+
+  userActiveSubscription:Subscription;
+  user1ActivatedIndct:boolean;
+  user2ActivatedIndct:boolean;
+
+  constructor(private userSvc:UserService){
+
+  }
+
+  ngOnInit(){
+
+   this.userActiveSubscription = this.userSvc.userActivated.subscribe((id:number)=>{
+      if(id===1){
+        this.user1ActivatedIndct = true;
+        this.user2ActivatedIndct = false;
+      }else {
+        this.user1ActivatedIndct = false;
+        this.user2ActivatedIndct = true;
+      }
+    })
+
+  }
+
+  ngOnDestroy(){
+    this.userActiveSubscription.unsubscribe();
+  }
+
 }
--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Subscription } from 'rxjs/Rx';
+import { observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  myNumbersSubscription:Subscription;
+  myCustomSUbscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+
+    const myNumbers = Observable.interval(1000);
+    this.myNumbersSubscription = myNumbers.subscribe((numbers:number)=>{
+      console.log(numbers);
+    });
+
+    const customSubscription = Observable.create((observer:Observer<string>)=>{
+      setTimeout(() => {
+        observer.next("first record")
+      }, 2000);
+      setTimeout(() => {
+        observer.next("Second record")
+      }, 4000);
+      // setTimeout(() => {
+      //   observer.error("errored out")
+      // }, 5000);
+      setTimeout(() => {
+        observer.complete();
+      }, 8000);
+      setTimeout(() => {
+        observer.next("third record")
+      }, 7000);
+
+      setTimeout(() => {
+        observer.next("fourth record")
+      }, 9000);
+    });
+
+    this,this.myCustomSUbscription = customSubscription.subscribe((value:string)=>
+    {
+      console.log(value);
+    })
+  }
+
+  ngOnDestroy() {
+    this.myNumbersSubscription.unsubscribe();
+    this.myCustomSUbscription.unsubscribe();
   }
 
 }
